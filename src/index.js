f684c684c7c25d0b3024bbf5cd5e286c06ae34a4
leadgfl13 <leadgfl13@gmail.com>
2025-02-06 15:34:22 -0500

    Append an image of a sprite from the fetch request

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 let search = document.getElementById("info");
 let submit = document.getElementById("submit");
+let sprite = document.getElementById("pokeimage");
 
 submit.addEventListener("click", (e) => {
 	e.preventDefault();
@@ -9,12 +10,19 @@ submit.addEventListener("click", (e) => {
 	getPokemon(search.value);
 });
 
+function addImage(poke) {
+	sprite.src = poke.sprites.front_shiny;
+}
+
 async function getPokemon(pokemon) {
 	const poke = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, {
 		mode: "cors",
 	})
 		.then((poke) => poke.json())
 		.then((poke) => {
-			console.log(poke);
+			addImage(poke);
 		});
+
+	let frontshiny = poke.sprites.front_shiny;
+	pokeimage.src = frontshiny;
 }
