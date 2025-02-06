94ed9d1b440715815723c17e096d227d0c19ee47
leadgfl13 <leadgfl13@gmail.com>
2025-02-05 21:11:20 -0500

    Make async function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import "./style.css";
 
+let search = document.getElementById("input");
 let submit = document.getElementById("submit");
 submit.addEventListener("click", (e) => {
 	e.preventDefault();
 	alert("howdy");
 });
+
+async function getPokemon(pokemon) {
+	const poke = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, {
+		mode: "cors",
+	})
+		.then((poke) => poke.json())
+		.then((poke) => {
+			console.log(poke);
+		});
+}
